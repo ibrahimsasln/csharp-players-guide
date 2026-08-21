45d844480a667815342ff5dd48f59b7792492903
İbrahim Salih Aslan <ibrahimsalihaslan@gmail.com>
2026-08-21 15:07:02 +0300

    add unrefactored version

--- SimulasTest/Program.cs
+++ SimulasTest/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿ChestState currentState = ChestState.Locked;
+bool exit = false;
+
+while (!exit)
+{
+    Console.Write($"The chest is {currentState}. What do you want to do? (0 for exit) ");
+    string? newState = Console.ReadLine(); // string? -> if newState is null
+    if (newState == null) continue;
+    if (currentState == ChestState.Locked && newState == "unlock") currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && newState == "open") currentState = ChestState.Open;
+    if (currentState == ChestState.Open && newState == "close") currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && newState == "lock") currentState = ChestState.Locked;
+    if(newState == "0") exit = true;
+}
+enum ChestState
+{
+    Open,
+    Closed,
+    Locked
+}
+
