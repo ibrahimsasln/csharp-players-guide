0c48504023c12f3c2db2c9d64ec10d13786245a4
İbrahim Salih Aslan <ibrahimsalihaslan@gmail.com>
2026-08-21 16:09:20 +0300

    set more accurate variable name

--- SimulasTest/Program.cs
+++ SimulasTest/Program.cs
@@ -4,10 +4,10 @@ bool exit = false;
 while (!exit)
 {
     Console.Write($"The chest is {currentState}. What do you want to do? (0 for exit) ");
-    string? newState = Console.ReadLine(); // string? -> if newState is null
-    if (newState == null) continue;
-    if(newState == "0") exit = true;
-    currentState = (currentState, newState) switch
+    string? userAction = Console.ReadLine(); // string? -> if newState is null
+    if (userAction == null) continue;
+    if(userAction == "0") exit = true;
+    currentState = (currentState, userAction) switch
     {
         (ChestState.Locked, "unlock") => ChestState.Closed,
         (ChestState.Closed, "open") => ChestState.Open,
