8d21a2b4ee598016714e1a6f90869f28f132c7ec
İbrahim Salih Aslan <ibrahimsalihaslan@gmail.com>
2026-08-27 15:27:45 +0300

    better solution

--- TheColor/Program.cs
+++ TheColor/Program.cs
@@ -1,55 +1,49 @@
 ﻿using System;
 using Microsoft.VisualBasic;
 
-namespace ProjeAdi
+namespace TheColor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Color color = new Color("Red");
-            Console.WriteLine($"({color.R}, {color.G}, {color.B})");
+            Color color1 = new Color();
+            Console.WriteLine($"({color1.R}, {color1.G}, {color1.B})");
+
+            Color color2 = Color.Blue;
+            Console.WriteLine($"({color2.R}, {color2.G}, {color2.B})");
+
+            Color color3 = new Color(2, 200, 20);
+            Console.WriteLine($"({color3.R}, {color3.G}, {color3.B})");
         }
     }
     
-    class Color
+    public class Color
+{
+    public byte R { get; }
+    public byte G { get; }
+    public byte B { get; }
+
+    public Color() //default: White(255, 255, 255)
+        {
+            R = 255;
+            G = 255;
+            B = 255;
+        }
+    public Color(byte r, byte g, byte b)
     {
-        public float R { get;}
-        public float G { get;}
-        public float B { get;}
-
-        public Color() // default: Black(0, 0, 0)
-        {
-            R = 0;
-            G = 0;
-            B = 0;
-        }
-
-        public Color(byte r, byte g, byte b)
-        {
-            R = r;
-            G = g;
-            B = b;
-        }
-
-        public Color(string i)
-        {
-            switch (i)
-            {
-                case "Red":
-                R = 255;
-                break;
-
-                case "Green":
-                G = 255;
-                break;
-
-                case "Blue":
-                B = 255;
-                break;
-            }
-
-        }
-
+        R = r;
+        G = g;
+        B = b;
     }
+
+    public static Color White  { get; } = new Color(255,  255,  255);
+    public static Color Black  { get; } = new Color(  0,    0,    0);
+    public static Color Red    { get; } = new Color(255,    0,    0);
+    public static Color Orange { get; } = new Color(255,  165,    0);
+    public static Color Yellow { get; } = new Color(255,  255,    0);
+    public static Color Green  { get; } = new Color(  0,  128,    0);
+    public static Color Blue   { get; } = new Color(  0,    0,  255);
+    public static Color Purple { get; } = new Color(128,    0,  128);
+}
 }
