a25bee6375fb4a1a78a5eda2f8b51b3bb3b4a6bd
İbrahim Salih Aslan <ibrahimsalihaslan@gmail.com>
2026-08-27 15:20:35 +0300

    first solution

--- TheColor/Program.cs
+++ TheColor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.VisualBasic;
 
 namespace ProjeAdi
 {
@@ -6,7 +7,49 @@ namespace ProjeAdi
     {
         static void Main(string[] args)
         {
-            
+            Color color = new Color("Red");
+            Console.WriteLine($"({color.R}, {color.G}, {color.B})");
         }
+    }
+    
+    class Color
+    {
+        public float R { get;}
+        public float G { get;}
+        public float B { get;}
+
+        public Color() // default: Black(0, 0, 0)
+        {
+            R = 0;
+            G = 0;
+            B = 0;
+        }
+
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public Color(string i)
+        {
+            switch (i)
+            {
+                case "Red":
+                R = 255;
+                break;
+
+                case "Green":
+                G = 255;
+                break;
+
+                case "Blue":
+                B = 255;
+                break;
+            }
+
+        }
+
     }
 }
