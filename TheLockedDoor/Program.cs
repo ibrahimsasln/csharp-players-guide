e4c27483beb4d65a50c84f7ff408301e3d0f8d14
İbrahim Salih Aslan <ibrahimsalihaslan@gmail.com>
2026-08-28 13:53:49 +0300

    working version need refactor

--- TheLockedDoor/Program.cs
+++ TheLockedDoor/Program.cs
@@ -6,31 +6,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Door door1 = new Door("12345");
+        door1.DoorAction(Action.Open);
     }
 }
 
 class Door
 {
-    public string Passcode { get; private set;}
-    public DoorState CurrentState { get; private set;} = DoorState.Locked;
+    public string Passcode { get; private set; }
+    public DoorState CurrentState { get; private set; } = DoorState.Locked;
     public Door(string passcode)
     {
         Passcode = passcode;
     }
 
-    public void DoorAction(string action)
+    public void DoorAction(Action action)
     {
-        if(CurrentState == DoorState.Locked)
+        while (true)
         {
-            Console.Write("Enter your passcode: ");
-            string? passcodeInput = Console.ReadLine();
-            if(passcodeInput == Passcode) CurrentState = DoorState.Closed;
-            else
+            if (CurrentState == DoorState.Locked)
             {
-                Console.WriteLine("Your passward is wrong");
-                return;
-            } 
+                Console.Write("Enter your passcode: ");
+                string? passcodeInput = Console.ReadLine();
+                if (passcodeInput == Passcode)
+                {
+                    CurrentState = DoorState.Closed;
+                    Console.WriteLine($"Unlocked. Now, door is {CurrentState}.");
+                }
+                else
+                {
+                    Console.WriteLine("Your passward is wrong");
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"Door is {CurrentState} Next move? (Open, Lock, Close)");
+            string? nextMove = Console.ReadLine();
+
+            if (nextMove != "Open" && nextMove != "Close" && nextMove != "Lock")
+            {
+                Console.WriteLine("Unexpected input. Try again.");
+                continue;
+            }
+
+            action = nextMove switch
+            {
+                "Open" => Action.Open,
+                "Lock" => Action.Lock,
+                "Close" => Action.Close,
+                _ => Action.Lock
+            };
+
+            CurrentState = (CurrentState, action) switch
+            {
+                (DoorState.Closed, Action.Open) => DoorState.Open,
+                (DoorState.Closed, Action.Lock) => DoorState.Locked,
+                (DoorState.Open, Action.Close) => DoorState.Closed,
+                _ => CurrentState
+            };
+            Console.WriteLine($"Door is {CurrentState}");
         }
 
     }
@@ -46,7 +80,6 @@ enum DoorState
 enum Action
 {
     Lock,
-    Unlock,
     Open,
     Close
 }
