bb09262aecf315ec5bc9b7c45129bf6098b8b7e8
İbrahim Salih Aslan <ibrahimsalihaslan@gmail.com>
2026-09-06 19:45:06 +0300

    add The Fountain of Objects need methods and more

---
+++ TheFountainOfObjects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TheFountainOfObjects;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Map
+{
+    //2D array for map 4x4
+}
+
+public class Player
+{
+    //can sense
+    // location property
+
+}
+
+public class PlayerInput
+{
+    
+}
+
+public class FountainOfObjectGame
+{
+    
+}
+
+public abstract class Room
+{
+    
+}
+
+public class FountainRoom : Room
+{
+    
+}
+
+public class CaveEnterance : Room
+{
+    
+}
